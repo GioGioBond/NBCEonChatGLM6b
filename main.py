8ea2eb5fabbe8b3133a7795f285a51ebb650389d
HoldTheSun <39087265+GIOGIOv587@users.noreply.github.com>
2023-06-02 17:03:38 +0800

    v1.1.0版本chatglm6b

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ import torch
 from transformers import AutoTokenizer,AutoModel
 
 # 模型路径 chatglm-6b
-model_path = '/opt/chatglm/chatglm_model/'
+model_path = ''
+
 
 # 加载tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_path,trust_remote_code=True)
@@ -19,7 +20,6 @@ tokenizer.padding_side = 'left'
 tokenizer.pad_token = tokenizer.unk_token
 
 # 加载chatglm-6b模型
-#model = AutoModel.from_pretrained(model_path, device_map='auto')
 model = AutoModel.from_pretrained(model_path, trust_remote_code=True).half().cuda()
 device = torch.device('cuda')
 
@@ -30,53 +30,41 @@ contexts = json.load(open('contexts.json'))
 question = """请仔细阅读材料，回答下面问题：
 - 创新药新巨头吉利德公司有多少个员工？
 - 领英计划裁员多少人？
-- 吉利德收购Pharmasset的价格是多少？
 """
 
 # 拼接context和question
 contexts = [''] + contexts  # 添加空Context（无Context预测）
-batch = ['User: %s\n\n%s\n\nAssistant:' % (context, question) for context in contexts]
+batch = ['User: %s\n\n%s\n\nAssistant:' % (context[:100], question) for context in contexts]
 
-#batch = batch[:4]
+batch = batch[:4]
 print('Context长度分布：', [len(text) for text in batch])
 print('Context总长度：', sum([len(text) for text in batch]))
-def get_attention_mask(attention_mask):
-
-    SHAPE = (len(attention_mask), 1, len(attention_mask[0]), len(attention_mask[0]))
-    _attention_mask = torch.zeros(SHAPE)
-
-    for i in range(len(attention_mask)):
-        for j in range(len(attention_mask[i])):
-            if attention_mask[i][j] == 0:
-                _attention_mask[i, 0, :, j] = 1
-            else:
-                _attention_mask[i, 0, j:, j] = 0
-    # print(_attention_mask)
-    return _attention_mask.bool().cuda()
+print(batch)
 
 @torch.inference_mode()
 def generate(max_tokens):
     """Naive Bayes-based Context Extension 演示代码
     """
+    eop_list= []
 
-    inputs = tokenizer(batch, padding='longest', return_tensors='pt', return_attention_mask=True).to(device)
+    inputs = tokenizer(batch, padding='longest', return_tensors='pt', return_attention_mask=True, skip_special_tokens=True).to(device)
     input_ids = inputs.input_ids
     attention_mask = inputs.attention_mask
 
+    res = ''
     n = input_ids.shape[0]
 
-    res = ''
     for i in range(max_tokens):
+
         # 模型输出
         #print(f'第{i+1}token开始输出')
-        _attention_mask = get_attention_mask(attention_mask)
-        #print(input_ids)
         outputs = model(input_ids=input_ids,
-                        attention_mask=_attention_mask,
+                        attention_mask=attention_mask,
                         return_dict=True,
                         use_cache=True
                        )
         torch.cuda.empty_cache()
+
         # ===== 核心代码开始 =====
         beta = 0.25
 
@@ -93,24 +81,30 @@ def generate(max_tokens):
 
         # 构建分布，采样
         # tau = 0.01  # tau = 1是标准的随机采样，tau->0则是贪心搜索
-        # _probas = torch.nn.functional.softmax(logits[None] / tau , dim=-1)
-        # _next_tokens = torch.multinomial(_probas, num_samples=1).squeeze(1)
 
         probas = torch.nn.functional.softmax(logits[None], dim=-1)
         next_tokens = torch.topk(probas,1).indices
 
         s = tokenizer.convert_ids_to_tokens(next_tokens)
-        #print(s)
+        print(s)
         res += s[0]
         if s[0] == '<eop>':
-            break
-
+            if len(eop_list)==3:
+                break
+            else:
+                eop_list.append('<eop>')
+        else:
+            eop_list = []
         # prepare for next iteration
         input_ids = torch.cat([input_ids, next_tokens.tile(n, 1)], dim=1)
-        attention_mask = torch.cat([attention_mask, torch.ones(n, 1, dtype=torch.long, device=device)], dim=-1)
+        attention_mask = torch.cat([attention_mask, torch.zeros(n, 1, 1, attention_mask.shape[-1], dtype=torch.long, device=device).bool()], dim=2)
+        attention_mask = torch.cat([attention_mask, torch.zeros(n, 1, attention_mask.shape[-2], 1, dtype=torch.long, device=device).bool()], dim=-1)
+
+
 
     print(question)
-    print(re.split('<n><n>',re.sub('▁|<eop>','',res)))
+    print(res)
+    print(re.split('<n>+',re.sub('▁|<eop>|<sop>','',res)))
     #['据公开报道,截至2021年6月,吉利德公司有约16,000名员工。', '领英计划裁员716人。', 'Pharmasset被吉利德以110亿美元收购。']
 if __name__ == '__main__':
     generate(1000)
